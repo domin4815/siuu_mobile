6289b1c890971ca01b3df2a2fc9b2a140bf81bdc
Robert Maguda <robert.maguda@gmail.com>
2016-06-14 22:14:28 +0200

    Created simple layout

---
+++ MyApplication/app/src/main/java/siuuklient/siuuclient/ApplicationUtils.java
@@ -0,0 +1,9 @@
+package siuuklient.siuuclient;
+
+/**
+ * Created by robert on 6/14/16.
+ */
+public class ApplicationUtils {
+    public static final String SERV_ADDR = "http://192.168.1.106:8080";
+    public static User user = new User("User1", 0.0, 0.0);
+}
---
+++ MyApplication/app/src/main/java/siuuklient/siuuclient/User.java
@@ -0,0 +1,52 @@
+package siuuklient.siuuclient;
+
+/**
+ * Created by robert on 6/14/16.
+ */
+public class User {
+
+    private Integer id = 1;
+
+    private String name;
+
+    private Double lat;
+    private Double lng;
+
+    public User(String name, Double lat, Double lng) {
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+}
